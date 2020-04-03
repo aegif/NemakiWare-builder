3b88475be08d6e99db9b8fc825dab230feeb45b2
U-DESKTOP-FE46BBI\riley <rileyduke@gmail.com>
2020-04-03 19:08:13 +0900

    update setup

--- image/files/setup.sh
+++ image/files/setup.sh
@@ -6,11 +6,13 @@ cd /app/nemakiware && mvn install
 cd /app/nemakiware/nemakiware/action && mvn install
 cd /app/nemakiware/common && mvn install
 
-# cd /app/nemakiware/ui && activator clean
-# cd /app/nemakiware/ui && activator war
+cd /app/nemakiware/ui && activator clean
+cd /app/nemakiware/ui && activator war
 
 cd /app/nemakiware && mvn clean package
 cd /app/nemakiware/core && mvn clean package
-# cd /app/nemakiware/solr && mvn clean package
+cd /app/nemakiware/solr && mvn clean package
 
 cp /app/nemakiware/core/target/core.war /share/core.war
+cp /app/nemakiware/ui/target/core.war /share/ui.war
+cp /app/nemakiware/solr/target/core.war /share/solr.war
